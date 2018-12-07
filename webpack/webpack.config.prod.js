21c826c265334cc26042ebf9158f6e5f9f997dfe
axel10 <axel10@163.com>
2018-12-07 15:11:52 +0800

    first

---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,36 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  stats: 'errors-only',
+  bail: true,
+  output: {
+    filename: 'js/[name].js?[chunkhash:16]',
+    chunkFilename: 'js/[name].chunk.js?[chunkhash:16]'
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css?[chunkhash:16]'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?css/i,
+        use : [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+});
